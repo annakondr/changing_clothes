a1138a10442f9911177a2f165fd5962ab3910801
annakondr <tyukondra@gmail.com>
2019-06-25 16:44:41 +0300

    Updates

---
+++ precache-manifest.cd6b74e71735bb4d7a44dd618c2d44f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e76f737c90844554bb0387d601ca5ad",
+    "url": "/changing-clothes/index.html"
+  },
+  {
+    "revision": "da7741e295e5b3efbc8e",
+    "url": "/changing-clothes/static/css/main.5a42a38e.chunk.css"
+  },
+  {
+    "revision": "78602b39524e5554df14",
+    "url": "/changing-clothes/static/js/2.f1b47934.chunk.js"
+  },
+  {
+    "revision": "da7741e295e5b3efbc8e",
+    "url": "/changing-clothes/static/js/main.cdf9ca40.chunk.js"
+  },
+  {
+    "revision": "e286c299200ab1c1691b",
+    "url": "/changing-clothes/static/js/runtime~main.f0aba13a.js"
+  }
+]);
