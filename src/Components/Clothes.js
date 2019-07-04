67e08e6f609ea46354d681af04335585d2362fff
annakondr <tyukondra@gmail.com>
2019-07-04 07:12:18 +0300

    css_add

--- src/Components/Clothes.js
+++ src/Components/Clothes.js
@@ -5,35 +5,38 @@ function Clothes(props) {
   const {clothes, edit, textInput, selected} = props;
 
   return (
-    <ul>
-      {clothes.map((item, index) => {
-        if (!edit || index !== selected) {
-          return (
-            <li key={item}>
-              <span className='item'>{item}</span>
-              <button onClick = {() => props.editClicked(index)}> Edit </button>
-            </li>)
-        } else {
-          return (
-            <li key={item} className={edit ? 'selected' : ''}>
-              <input type='text' defaultValue={item}
-                     onChange={(event) => props.inputChanged(event.target.value)}
-                     onKeyDown={(event) => {
-                       if(event.key === 'Enter') {
-                         props.clickEnter(textInput)
-                       } else if(event.key === 'Escape') {
-                         props.clickEsc()
+    <div className="block">
+      <ul>
+        {clothes.map((item, index) => {
+          if (!edit || index !== selected) {
+            return (
+              <li key={item}>
+                <span className="item">{item}</span>
+                <button onClick = {() => props.editClicked(index)}> Edit </button>
+              </li>)
+          } else {
+            return (
+              <li key={item} className={edit ? 'selected' : ''}>
+                <input type='text' defaultValue={item}
+                       onChange={(event) => props.inputChanged(event.target.value)}
+                       onKeyDown={(event) => {
+                         if(event.key === 'Enter') {
+                           props.clickEnter(textInput)
+                         } else if(event.key === 'Escape') {
+                           props.clickEsc()
+                         }
                        }
-                     }
-                     }
-              />
-              <button disabled={true}> Edit </button>
-            </li>
-          )
+                       }
+                />
+                <button disabled={true}> Edit </button>
+              </li>
+            )
+          }
+        })
         }
-      })
-      }
-    </ul>
+      </ul>
+    </div>
+
   )
 }
 
